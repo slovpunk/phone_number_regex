b5cf3f5df5d3abf0fd74e6ba7bd37d7f8d3f6dce
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-09 18:39:36 +0300

    fix bags

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -18,35 +17,28 @@ public class Main {
         String blackList = null;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("blacklist.txt"));
-        BufferedReader bufferedReader1 = new BufferedReader(new FileReader("whitelist.txt"))) {
+             BufferedReader bufferedReader1 = new BufferedReader(new FileReader("whitelist.txt"))) {
             String blackline;
             String whiteline;
             while ((blackline = bufferedReader.readLine()) != null) {
                 blackList = blackline;
             }
-            while((whiteline = bufferedReader1.readLine()) != null) {
+            while ((whiteline = bufferedReader1.readLine()) != null) {
                 whiteList = whiteline;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-
         Pattern blacklistPattern = numbers.createRegexPattern(blackList);
         Pattern whitelistPattern = numbers.createRegexPattern(whiteList);
 
-
-//        String[] msisdns = {"2345646", "4562123", "1231323"};
-
-//        for (String str : msisdns) {
-//            if (whitelistPattern.matcher(str).lookingAt() && !blacklistPattern.matcher(str).lookingAt()) {
-//                System.out.println(str + " можете купить");
-//            } else {
-//                System.out.println(str + " не можете купить");
-//            }
-//        }
+        while (!number.equals("exit")) {
+            if (whitelistPattern.matcher(number).lookingAt() && !blacklistPattern.matcher(number).lookingAt()) {
+                System.out.println("Номер доступен, отсутствует в черном списке и есть в белом");
+                continue;
+            }
+        }
     }
 }
