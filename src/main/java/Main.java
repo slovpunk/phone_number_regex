196abe7d4a76a8447d43e5f8a01c3c73905235c2
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-09 18:28:17 +0300

    add bufferedreader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Numbers numbers = new NumbersImpl();
 
-        String whiteList = "444,4562123,555";
-        String blackList = "345678,2345646,1231323";
+        //enter the desired number
+        String number = scanner.nextLine();
+
+        String whiteList = null;
+        String blackList = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("blacklist.txt"));
+        BufferedReader bufferedReader1 = new BufferedReader(new FileReader("whitelist.txt"))) {
+            String blackline;
+            String whiteline;
+            while ((blackline = bufferedReader.readLine()) != null) {
+                blackList = blackline;
+            }
+            while((whiteline = bufferedReader1.readLine()) != null) {
+                whiteList = whiteline;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         Pattern blacklistPattern = numbers.createRegexPattern(blackList);
         Pattern whitelistPattern = numbers.createRegexPattern(whiteList);
 
 
-        String[] msisdns = {"2345646", "4562123", "1231323"};
+//        String[] msisdns = {"2345646", "4562123", "1231323"};
 
-        boolean isWhiteList = whiteList == null || whiteList.isEmpty();
-
-        for (String str : msisdns) {
-            if ((whitelistPattern.matcher(str).lookingAt() && isWhiteList) && !blacklistPattern.matcher(str).lookingAt()) {
-                System.out.println(str + " можете купить");
-            } else {
-                System.out.println(str + " не можете купить");
-            }
-        }
+//        for (String str : msisdns) {
+//            if (whitelistPattern.matcher(str).lookingAt() && !blacklistPattern.matcher(str).lookingAt()) {
+//                System.out.println(str + " можете купить");
+//            } else {
+//                System.out.println(str + " не можете купить");
+//            }
+//        }
     }
 }
